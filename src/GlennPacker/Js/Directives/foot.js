abfd52cbd92003963e54d41e04ec81b3218a2045
glenn packer <glenn.packer@yahoo.co.uk>
2017-08-10 19:50:07 +0100

    wording, footer, layout
    wording,
    footer,
    home page layout

---
+++ src/GlennPacker/Js/Directives/foot.js
@@ -0,0 +1,12 @@
+﻿var app = angular.module('app.directives');
+
+app.directive('foot', [function() {
+    return {
+        restrict: 'E',
+        replace: true,
+        transclude: false,
+        templateUrl: '/js/templates/Footer.html',
+        scope: {},
+        link: function postLink(scope) {}
+    };
+}])
